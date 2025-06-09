fbd5210ba0e3b943cd9bd66415feb4cd1b770e6f
Artemka <sikachyovartem@yandex.ru>
2025-06-09 23:49:52 +0300

    Обновил создание пользователя.

--- orpud_project/orpud_project/settings.py
+++ orpud_project/orpud_project/settings.py
@@ -215,3 +215,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+
+DJOSER = {
+    'USER_CREATE_PASSWORD_RETYPE': False,
+    'SERIALIZERS': {
+        'user_create': 'user.serializers.CustomUserCreateSerializer',
+    }
+}
