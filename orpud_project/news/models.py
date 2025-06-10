098f5c33ab037be769ef1a42f75cfc139dd1d788
Artemka <sikachyovartem@yandex.ru>
2025-06-10 17:08:33 +0300

    Фикс

--- orpud_project/news/models.py
+++ orpud_project/news/models.py
@@ -16,6 +16,9 @@ class TimestampModel(models.Model):
 class Tag(models.Model):
     title = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.title
+
 
 class News(TimestampModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news")
